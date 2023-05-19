476039a4b6f33302adcae496c4a5cce0dbb7c581
NithinKM23 <nithinkm55@gmail.com>
2023-05-20 04:26:53 +0530

    4

--- to_do.js
+++ to_do.js
@@ -7,15 +7,47 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     let tableData = "";
     objectData.map((values) => {
       tableData += `<tr>
-      <td>${values.id}</td>
+      <td>${values.id}</td>  
       <td>${values.title}</td>
-      <td>${values.completed}</td>
+      <td>
+      <div class="form-check">
+      <input class="form-check-input status" type="checkbox" value="values.title" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">  </label>
+    </div></td>
     
     </tr>`;
     });
 
-    document.getElementById("table_body").innerHTML=tableData;
+    // // let statSel=[]
+    // function checking()
+
+    // {
+    //     // statSel.length=0;
+    //     for(c of checkboxes){
+    //         // if(c.checked){
+    //         //     statSel.push(c.value)
+    //         // }
+    //         console.log(c.checked)
+    //     }
+    //     // if(statSel.length>=5){
+    //     //     console.log(statSel)
+    //     //     alert( `“ Congrats. 5 Tasks have been Successfully Completed ”` )
+    //     // }
+    // }
+
+    document.getElementById("table_body").innerHTML = tableData;
   })
-  .catch((err)=>{
-    console.log(error)
-  })
+  .catch((err) => {
+    console.log(error);
+  });
+let checkboxes = document.querySelector(".status");
+checkboxes.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let stat = [];
+  document.querySelectorAll("[type='checkbox']").forEach((item) => {
+    if (item.checked === true) {
+      stat.push(item.value);
+    }
+  });
+  console.log(stat);
+});
