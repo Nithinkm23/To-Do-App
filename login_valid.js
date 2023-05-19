0a29af13057bd522cf13d99eb9d36ba1e14630c5
NithinKM23 <nithinkm55@gmail.com>
2023-05-19 23:44:48 +0530

    initial

---
+++ login_valid.js
@@ -0,0 +1,53 @@
+eml = document.getElementById("email");
+var passw = document.getElementById("password");
+
+var error2 = document.getElementById("err2");
+var error3 = document.getElementById("err3");
+
+
+
+  document.getElementById("login").onclick = function redirect() {
+
+    if (eml.value == "admin" && passw.value == "12345") {
+
+    window.location.href = "http://127.0.0.1:5500/mainPage.html";
+    alert("Logging in");
+  }
+  else{
+alert("OOPS!!")
+  }
+}
+// function redirect() {
+//   if (eml.value == "admin" && passw.value == "12345") {
+
+//     btn.addEventListener("click", () => {
+//       window.location= "http://127.0.0.1:5500/mainPage.html";
+//     });
+//   }
+// }
+
+function validateemail() {
+  if (eml.value == "admin") {
+    error2.innerText = "Valid username";
+    error2.style.color = "green";
+    return true;
+  } else {
+    error2.innerText = "Invalid username ";
+    error2.style.color = "red";
+    alert("Enter valid username ");
+    return false;
+  }
+}
+
+function validatepassword() {
+  if (passw.value == "12345") {
+    error3.innerText = "Valid Password ";
+    error3.style.color = "green";
+    return true;
+  } else {
+    error3.innerText = "Invalid Password ";
+    alert("Password should contain Minimum 5 characters");
+    error3.style.color = "red";
+    return false;
+  }
+}
